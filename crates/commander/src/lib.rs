4307f05a6478767c2e44853cf86b3de18e2e50cd
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-22 18:32:34 -0500

    crates/commander:rest subcommand

--- crates/commander/src/lib.rs
+++ crates/commander/src/lib.rs
@@ -1,10 +1,9 @@
-//lib.rs
 use rest::rest;
 mod rest;
-
 pub fn run(name: &str) {
     match name {
         "rest" => rest(),
+        //"rest" => rest(),
         _ => panic!("Invalid option"),
     };
 }
--- crates/commander/src/main.rs
+++ crates/commander/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .version("v0.0.1")
         .setting(AppSettings::ArgRequiredElseHelp)
         .subcommand(SubCommand::with_name("rest"))
+        //.subcommand(SubCommand::with_name("rest"))
         .get_matches();
 
     match matches.subcommand_name() {
