5626c80c7244b9553d001cf4eef3559b5737baba
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-29 12:32:40 -0500

    crates/commander

--- crates/commander/src/lib.rs
+++ crates/commander/src/lib.rs
@@ -1,9 +1,11 @@
+use online::online;
+mod online;
 use rest::rest;
 mod rest;
 pub fn run(name: &str) {
     match name {
         "rest" => rest(),
-        //"rest" => rest(),
+        "online" => online(),
         _ => panic!("Invalid option"),
     };
 }
