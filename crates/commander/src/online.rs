a48518615421153441972bd224fdfa202f5865ec
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-29 14:08:40 -0500

    crates/commander

--- crates/commander/src/online.rs
+++ crates/commander/src/online.rs
@@ -1,35 +1,34 @@
-use reqwest::Error;
-use reqwest::Url;
-use serde::{Deserialize, Serialize};
-
-
 use std::io::Read;
 use std::process;
+
+use reqwest::{Error, Url};
+use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
 
 pub(crate) fn online() {
-
-   let url = Url::parse("https://api.nostr.watch/v1/online").unwrap();
+    let url = Url::parse("https://api.nostr.watch/v1/online").unwrap();
     let mut res = reqwest::get(url).unwrap();
 
     let mut tmp_string = String::new();
     res.read_to_string(&mut tmp_string).unwrap().to_string();
     //println!("{:}", format!("{:}", tmp_string.to_string()));
-    tmp_string = tmp_string.replace("[","");
-    tmp_string = tmp_string.replace("]","");
+    tmp_string = tmp_string.replace("[", "");
+    tmp_string = tmp_string.replace("]", "");
     let v: Vec<&str> = tmp_string.split(",").collect();
     //print!("{:?}", v);
+    //println!("{:?}", v[0]);
+    //println!("{:?}", v[1]);
+    let mut count = 0;
     for relay in v {
-    print!("{:} ", relay);
-
-}
+        println!("{},{:} ", count, relay);
+        count += 1;
+    }
     //Ok(tmp_string)
 
     //let relay: Relay = serde_json::from_str(&tmp_string).expect("REASON");
     //println!("relay: {:?}", relay);
 
-
-process::exit(0);
+    process::exit(0);
     let request_url = "https://jsonplaceholder.typicode.com/photos";
     let mut response = reqwest::get(request_url).unwrap();
     let json = response.json::<Vec<Photo>>().unwrap();
